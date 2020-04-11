f4965c7c8e9e30118b475a7cfa09ea132b15fc22
Kyle Van Meter <kvanmete76751@gmail.com>
2020-04-11 00:17:42 -0600

    index.ts: Fix CORS, Added HTTPS
    Adding HTTPS will finally allow us to use WebAudioAPI stuff in DoNothing

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
-import * as Ex from 'express';
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
 import fs, { ReadStream } from 'fs';
+import https from 'https';
 
 const app: express.Application = express();
 const audioExt: Set<String> = new Set([
@@ -18,6 +18,11 @@ const audioExt: Set<String> = new Set([
 const port: number = 3001;
 
 app.use(cors());
+app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+});
+
 app.use((req: express.Request, res: express.Response) => {
     const reqSplit: String[] = req.url.split('.');
     const reqExt: String = reqSplit[reqSplit.length - 1];
@@ -35,4 +40,9 @@ app.use((req: express.Request, res: express.Response) => {
     }
 });
 
-app.listen(port, () => console.log(`Server listening on port ${port}`));
+https.createServer({
+    key: fs.readFileSync('./key.pem'),
+    cert: fs.readFileSync('./cert.pem'),
+    passphrase: `"ENTER_PASSPHRAZE!"`
+}, app).listen(port, () => console.log(`Server listening on port ${port}`));
+//app.listen(port, () => console.log(`Server listening on port ${port}`));
